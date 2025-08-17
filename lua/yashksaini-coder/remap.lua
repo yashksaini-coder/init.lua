d2c959be1b02a6af7fe73d83548260d1ba5cb799
yashksaini-coder <yashksaini89@gmail.com>
2025-08-17 20:44:07 +0530

    Add Git Blame key mappings and theme switching functionality in remap.lua; update lazy-lock.json with new plugin dependencies

--- lua/yashksaini-coder/remap.lua
+++ lua/yashksaini-coder/remap.lua
@@ -9,3 +9,33 @@ vim.keymap.set("n", "<leader>li", "<cmd>Lazy install<CR>", { desc = "Install plu
 vim.keymap.set("n", "<leader>lc", "<cmd>Lazy check<CR>", { desc = "Check plugin health" })
 vim.keymap.set("n", "<leader>lx", "<cmd>Lazy clean<CR>", { desc = "Remove unused plugins" })
 
+-- 🔎 Git Blame Keymaps
+	-- Toggle inline blame annotations
+vim.keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>", { desc = "Toggle Git Blame" })
+
+	-- Copy commit SHA of current line
+vim.keymap.set("n", "<leader>gc", ":GitBlameCopySHA<CR>", { desc = "Copy Git Blame SHA" })
+
+	-- Copy commit URL (if remote origin is set, e.g. GitHub)
+vim.keymap.set("n", "<leader>gu", ":GitBlameCopyCommitURL<CR>", { desc = "Copy Git Blame URL" })
+
+	-- Open commit in browser (GitHub/GitLab/etc.)
+vim.keymap.set("n", "<leader>go", ":GitBlameOpenCommitURL<CR>", { desc = "Open Commit in Browser" })
+
+
+-- Switching themes keymaps
+
+	-- Switch to Chai theme
+vim.keymap.set("n", "<leader>ct", function()
+  require("chai").setup({
+	transparent_background=true,
+  })
+  vim.cmd("colorscheme chai")
+end, { desc = "Switch to Chai theme" })
+
+	-- Switch back to TokyoNight
+vim.keymap.set("n", "<leader>tt", function()
+  require("tokyonight").setup({ style = "storm", transparent = false })
+  vim.cmd("colorscheme tokyonight")
+end, { desc = "Switch to TokyoNight theme" })
+
